beb9b66e576ebaa616f2994094a4f58de95d6c6c
季楠 <2759278070@qq.com>
2025-01-01 17:47:38 +0800

    [+]修改文档内容

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,6 +4,9 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import Layout from './Layout.vue'
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { useData, useRoute } from 'vitepress';
+import { toRefs } from "vue";
 
 export default {
   extends: DefaultTheme,
@@ -15,5 +18,32 @@ export default {
   // },
   enhanceApp({ app, router, siteData }) {
     // ...
+  },
+  setup() {
+    // 获取前言和路由
+    const { frontmatter } = toRefs(useData());
+    const route = useRoute();
+
+    // 评论组件 - https://giscus.app/
+    giscusTalk({
+          repo: 'CPJiNan/Wiki',
+          repoId: 'R_kgDOMHbYuQ',
+          category: 'Announcements',
+          categoryId: 'DIC_kwDOMHbYuc4Clpmk',
+          mapping: 'pathname',
+          inputPosition: 'top',
+          lang: 'zh-CN',
+          locales: {
+            'zh-Hans': 'zh-CN',
+            'en-US': 'en'
+          },
+          homePageShowComment: false,
+          lightTheme: 'light',
+          darkTheme: 'transparent_dark',
+        }, {
+          frontmatter, route
+        },
+        true
+    );
   }
 } satisfies Theme
